86cfe6fac0711d95c1eeecdaaf0a8e547b3e6b24
Md.Noman Hasan <nomanhasan2226@gmail.com>
2022-09-22 17:53:50 -0700

    fgfg

--- helpers/environments.js
+++ helpers/environments.js
@@ -1,13 +1,23 @@
 const environments = {};
  environments.staging = {
     port : 3000,
+<<<<<<< HEAD
     envName : 'staging'
+=======
+    envName : 'staging',
+    secretKey : 'sdfwfsdfsdfsdf',
+>>>>>>> b81abe2 (fgfg)
 
  };
  
  environments.production = {
     port : 5000,
+<<<<<<< HEAD
     envName : 'production'
+=======
+    envName : 'production',
+    secretKey : 'sdfwfdfvfsdfsdfsdggf',
+>>>>>>> b81abe2 (fgfg)
  };
 
  const currentEnvironment= typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : 'staging';
